949b73a59ea99ee957a2775c85217d9c26d35e05
Thierry <thiernkenfack@gmail.com>
2026-06-05 19:08:03 +0200

    Modification et application du domaine greenops dans frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -128,8 +128,8 @@
 <script>
 import axios from 'axios';
 
-const API_BASE = "http://localhost:8000/api";
-// const API_BASE = "http://greenops.local:51247/api";
+// const API_BASE = "http://localhost:8000/api";
+const API_BASE = "http://greenops.test/api";
 
 export default {
   data() {
